fabc36fd3072bddb8e0c70eaada7965b7f8830ad
Doan Adrien <adoan@2D-LINUX-027.insa-lyon.fr>
2025-03-18 09:40:22 +0100

    Branche Adrien

--- compiler/config.mk
+++ compiler/config.mk
@@ -1,3 +1,4 @@
 ANTLRJAR=/home/$(USER)/antlr4-install/antlr-4.13.2-complete.jar
-ANTLRINC=/home/$(USER)/antlr4-install/build/usr/local/include/antlr4-runtime/
-ANTLRLIB=/home/$(USER)/antlr4-install/build/usr/local/lib/libantlr4-runtime.a
+ANTLRINC=/home/adoan/antlr4-install/include/antlr4-runtime
+ANTLRLIB=/home/adoan/antlr4-install/lib/libantlr4-runtime.a
+
