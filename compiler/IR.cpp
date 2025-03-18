db6742dbd3c7f188fbb036ecac8ae9c0549b32e9
Doan Adrien <adoan@2D-LINUX-097.insa-lyon.fr>
2025-03-18 10:45:16 +0100

    BasicCLock implementation

--- compiler/IR.cpp
+++ compiler/IR.cpp
@@ -1,1 +1,15 @@
-//
+#include "IR.h"
+
+BasicBlock::BasicBlock(CFG* cfg, string entry_label){
+    label = entry_label;
+    cfg = cfg;
+}
+
+void BasicBlock::gen_asm(ostream &o){
+    for_each(instrs.begin(), instrs.end(), gen_asm(o));
+} /**< x86 assembly code generation for this basic block (very simple) */
+
+void BasicBlock::add_IRInstr(IRInstr::Operation op, Type t, vector<string> params){
+    IRInstr ins = IRInstr(this, op, t, params);
+    instrs.push_back(ins);
+}
