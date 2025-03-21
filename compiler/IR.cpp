1fda585af64d99b67a8fc168488e68700f3b2139
ZenThe0 <adrien.doan@insa-lyon.fr>
2025-03-21 08:42:24 +0100

    BasicBlock asm

--- compiler/IR.cpp
+++ compiler/IR.cpp
@@ -6,7 +6,19 @@ BasicBlock::BasicBlock(CFG* cfg, string entry_label){
 }
 
 void BasicBlock::gen_asm(ostream &o){
+    std::cout<< "@" << label <<":\n";
     for_each(instrs.begin(), instrs.end(), gen_asm(o));
+    if(exit_true  && exit_false){
+        std::cout<< "   cmp  $0, " << test_var_name << " \n";
+        std::cout<< "   je " << exit_false->label << " \n";
+        std::cout<< "   jmp @" << exit_true->label << " \n";
+    }
+    else if(exit_true && !exit_false){
+        std::cout<< "   jmp @" << exit_true->label << " \n";
+    }
+    else{
+
+    }
 } /**< x86 assembly code generation for this basic block (very simple) */
 
 void BasicBlock::add_IRInstr(IRInstr::Operation op, Type t, vector<string> params){
