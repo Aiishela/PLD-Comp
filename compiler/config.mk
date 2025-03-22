a290f7186233fc154f2ab365a2b8ef2b9c0d7a16
Aiishela <mbaratova15@gmail.com>
2025-03-22 10:47:41 +0100

    Stop tracking compiler/config.mk

--- compiler/config.mk
+++
@@ -1,3 +0,0 @@
-ANTLRJAR=/home/$(USER)/antlr4-install/antlr-4.13.2-complete.jar
-ANTLRINC=/home/simtri334/antlr4-install/include/antlr4-runtime
-ANTLRLIB=/home/simtri334/antlr4-install/lib/libantlr4-runtime.a
