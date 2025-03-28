5a8d8c13f4f496d1e2342e354e3a334a9b53752b
Aiishela <mbaratova15@gmail.com>
2025-03-28 12:02:24 +0100

    variablecheckvisitor

---
+++ compiler/VariableCheckVisitor.cpp
@@ -0,0 +1,169 @@
+#include "VariableCheckVisitor.h"
+extern SymbolTable * symbolTable;
+
+antlrcpp::Any VariableCheckVisitor::visitFunc(ifccParser::FuncContext *ctx) 
+{
+    CFG * cfg = new CFG(ctx->VAR()->getText());
+    listCFG->push_back(cfg);
+
+    for(ifccParser::StmtContext * i : ctx->stmt()) this->visit( i );
+    this->visit( ctx->return_stmt() );
+
+    return 0;
+}
+
+// ------------------------------------------ RETURN -----------------------------------------
+
+antlrcpp::Any VariableCheckVisitor::visitReturn_stmt(ifccParser::Return_stmtContext *ctx)
+{
+    this->visit( ctx->expr() );
+
+    return 0;
+}
+
+// ------------------------------------------ EXPR -----------------------------------------
+
+antlrcpp::Any VariableCheckVisitor::visitExprconst(ifccParser::ExprconstContext *ctx) {
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprvar(ifccParser::ExprvarContext *ctx) {
+    int line = ctx->getStart()->getLine();
+    int col = ctx->getStart()->getCharPositionInLine();
+    std::string var = ctx->VAR()->getText();
+    (*listCFG->rbegin())->symbolTable->useVariable(var, line, col);
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprchar(ifccParser::ExprcharContext *ctx) {
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprbracket(ifccParser::ExprbracketContext *ctx) {
+    this->visit( ctx->expr() );
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprunaire(ifccParser::ExprunaireContext *ctx) {
+    this->visit( ctx->expr() );
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprmuldivmod(ifccParser::ExprmuldivmodContext *ctx)  {
+    this->visit( ctx->expr()[0] );
+    this->visit( ctx->expr()[1] );
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprcomplg(ifccParser::ExprcomplgContext *ctx) {
+    this->visit( ctx->expr()[0] );
+    this->visit( ctx->expr()[1] );
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprcompeqdiff(ifccParser::ExprcompeqdiffContext *ctx) {
+    this->visit( ctx->expr()[0] );
+    this->visit( ctx->expr()[1] );
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExpraddsub(ifccParser::ExpraddsubContext *ctx) {
+    this->visit( ctx->expr()[0] );
+    this->visit( ctx->expr()[1] ); 
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprandbb(ifccParser::ExprandbbContext *ctx) {
+    this->visit( ctx->expr()[0] );
+    this->visit( ctx->expr()[1] ); 
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprnotbb(ifccParser::ExprnotbbContext *ctx) {
+    this->visit( ctx->expr() ); 
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExprorbb(ifccParser::ExprorbbContext *ctx) {
+    this->visit( ctx->expr()[0] );
+    this->visit( ctx->expr()[1] ); 
+
+    return 0;
+}
+
+// ------------------------------------------ DECLARATION -------------------------------------
+
+antlrcpp::Any VariableCheckVisitor::visitDeclaration(ifccParser::DeclarationContext *ctx) {
+    this->visit( ctx->decl() );
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitDeclexpr(ifccParser::DeclexprContext *ctx) { 
+    int line = ctx->getStart()->getLine();
+    int col = ctx->getStart()->getCharPositionInLine();
+
+    this->visit( ctx->expr() );
+    std::string var = ctx->VAR()->getText();
+
+    (*listCFG->rbegin())->symbolTable->addVariable(var, INT, line, col);
+    (*listCFG->rbegin())->symbolTable->defineVariable(var, line, col);
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitDeclchar(ifccParser::DeclcharContext *ctx) {
+    int line = ctx->getStart()->getLine();
+    int col = ctx->getStart()->getCharPositionInLine();
+    string var = ctx->VAR()->getText();
+    
+    (*listCFG->rbegin())->symbolTable->addVariable(var, CHAR, line, col);
+    (*listCFG->rbegin())->symbolTable->defineVariable(var, line, col);
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitDeclalone(ifccParser::DeclaloneContext *ctx) { 
+    Type t;
+    if ( (ctx->type->getText()).compare("int") == 0) {
+        t = INT;
+
+    } else {
+        t = CHAR;
+    }
+
+    for(antlr4::tree::TerminalNode * i : ctx->VAR()) {
+        int line = ctx->getStart()->getLine();
+        int col = ctx->getStart()->getCharPositionInLine();
+        std::string var = i->getText();
+        (*listCFG->rbegin())->symbolTable->addVariable(var,t, line, col);
+    }
+
+    return 0;
+}
+
+// --------------------------------------- AFFFECTATION --------------------------------
+
+antlrcpp::Any VariableCheckVisitor::visitExpression(ifccParser::ExpressionContext *ctx) {
+    this->visit( ctx->expr() );
+
+    return 0;
+}
+
+antlrcpp::Any VariableCheckVisitor::visitExpraff(ifccParser::ExpraffContext *ctx) {
+    int line = ctx->getStart()->getLine();
+    int col = ctx->getStart()->getCharPositionInLine();
+    std::any_cast<int>(this->visit( ctx->expr() ));
+    std::string var = ctx->VAR()->getText();
+
+    (*listCFG->rbegin())->symbolTable->defineVariable(var, line, col);
+ 
+    return 0;
+}
---
+++ compiler/VariableCheckVisitor.h
@@ -0,0 +1,51 @@
+#pragma once
+
+
+#include "antlr4-runtime.h"
+#include "generated/ifccBaseVisitor.h"
+#include "SymbolTable.h"
+#include <map>
+#include <string>
+#include <list>
+#include "CFG.h"
+
+using namespace std;
+
+class  VariableCheckVisitor : public ifccBaseVisitor {
+    public:
+        VariableCheckVisitor() : listCFG(new list<CFG *>) {}
+
+        virtual antlrcpp::Any visitFunc(ifccParser::FuncContext *ctx) override ; 
+
+        // RETURN 
+        virtual antlrcpp::Any visitReturn_stmt(ifccParser::Return_stmtContext *ctx) override;
+
+        // EXPR
+        virtual antlrcpp::Any visitExprconst(ifccParser::ExprconstContext *ctx) override ;
+        virtual antlrcpp::Any visitExprvar(ifccParser::ExprvarContext *ctx) override ;
+        virtual antlrcpp::Any visitExprchar(ifccParser::ExprcharContext *ctx) override ; 
+        virtual antlrcpp::Any visitExprunaire(ifccParser::ExprunaireContext *ctx) override ;
+        virtual antlrcpp::Any visitExprbracket(ifccParser::ExprbracketContext *ctx) override ;
+        virtual antlrcpp::Any visitExprmuldivmod(ifccParser::ExprmuldivmodContext *ctx) override ;
+        virtual antlrcpp::Any visitExprcomplg(ifccParser::ExprcomplgContext *ctx) override ;
+        virtual antlrcpp::Any visitExprcompeqdiff(ifccParser::ExprcompeqdiffContext *ctx) override ;
+        virtual antlrcpp::Any visitExpraddsub(ifccParser::ExpraddsubContext *ctx) override ;
+        virtual antlrcpp::Any visitExprandbb(ifccParser::ExprandbbContext *ctx) override ;
+        virtual antlrcpp::Any visitExprnotbb(ifccParser::ExprnotbbContext *ctx) override ;
+        virtual antlrcpp::Any visitExprorbb(ifccParser::ExprorbbContext *ctx) override ;
+
+        // DECLARATION
+        virtual antlrcpp::Any visitDeclaration(ifccParser::DeclarationContext *ctx) override ;
+        virtual antlrcpp::Any visitDeclexpr(ifccParser::DeclexprContext *ctx) override ;  
+        virtual antlrcpp::Any visitDeclchar(ifccParser::DeclcharContext *ctx) override ;  
+        virtual antlrcpp::Any visitDeclalone(ifccParser::DeclaloneContext *ctx) override ;
+
+        // AFFECTATION
+        virtual antlrcpp::Any visitExpression(ifccParser::ExpressionContext *ctx) override ; 
+        virtual antlrcpp::Any visitExpraff(ifccParser::ExpraffContext *ctx) override ; 
+
+    // protected:
+        list<CFG *> * listCFG;
+
+};
+
