f3b3e6eebd5e90f33d3dad84083e3336b503983e
Aiishela <mbaratova15@gmail.com>
2025-04-01 21:34:09 +0200

    gestion mémoire quand on passe dans le if + tests if

---
+++ testfiles/16_Structures_de_controle/16_9.c
@@ -0,0 +1,12 @@
+int main (){
+    int var1 = 0;
+    int var2;
+    if(var1 == 0){
+        int a = 2;
+        var2 = 4;
+    } else {
+        int c = 2; 
+    }
+    int b = c + 1 ;
+    return c + var2;    // error : c is not declared
+}
