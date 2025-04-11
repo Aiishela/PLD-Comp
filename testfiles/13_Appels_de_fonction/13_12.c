04ad38ffba8719481be746231607a634eabe2a4e
nadine-saadalla <nadine.saadalla@insa-lyon.fr>
2025-04-11 10:01:08 +0200

    check des appels de fonctions

---
+++ testfiles/13_Appels_de_fonction/13_12.c
@@ -0,0 +1,15 @@
+void func() {
+    putchar('A'); 
+    putchar('B'); 
+    putchar('C'); 
+    putchar('D'); 
+    putchar('E'); 
+    putchar('F'); 
+    putchar('G'); 
+}
+
+int main(){
+    func();
+    return 7;
+}
+
---
+++ testfiles/13_Appels_de_fonction/13_9_same_name.c
@@ -0,0 +1,15 @@
+int multi(int x, int y, int z){
+    int a = x*y*z;
+    return a;
+}
+
+int main(){
+    int b = multi(2,3,5);
+    int a = 2;
+    return a;
+}
+
+int multi(int x, int y){
+    int a = x*y;
+    return a;
+}
