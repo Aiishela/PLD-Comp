a9c40ede75194ece2b1b15518d29183e7813e394
Aiishela <75840940+Aiishela@users.noreply.github.com>
2025-04-14 17:17:50 +0200

    DO NOT COMMIT compiler/config.mk

--- compiler/config.mk
+++
@@ -1,3 +0,0 @@
-ANTLRJAR=/home/$(USER)/antlr4-install/antlr-4.13.2-complete.jar
-ANTLRINC=/home/adoan/antlr4-install/include/antlr4-runtime
-ANTLRLIB=/home/adoan/antlr4-install/build/runtime/libantlr4-runtime.a
